a6acd12b3f99b3ac992a27ac5d28025b9056469e
PEM-- <pemarchandet@gmail.com>
2019-01-23 16:25:48 +0100

    Styled-components & normalize.css

--- client/containers/Main.tsx
+++ client/containers/Main.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 
+import { GlobalStyle } from "../themes/GlobalStyle"
+
 export interface MainProps { compiler: string; framework: string; }
 
 export class Main extends React.Component<MainProps, {}> {
   render() {
-    return <h1>Test component: {this.props.compiler}, {this.props.framework}</h1>;
+    return (<React.Fragment>
+        <GlobalStyle />
+        <h1>Test component: {this.props.compiler}, {this.props.framework}</h1>
+      </React.Fragment>);
   }
 }
